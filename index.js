f5223fb0ddfe5f5acc8f4bc2cd72a2cc697cf014
yisangho <yisangho717@gmail.com>
2018-09-05 00:21:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function getFirstSelector(selector) {
   var ret = document.querySelector(selector)[0];
   return ret;
 }
+
+function nestedTarget() {
+  const lis = document
+  .getElementById("nested")
+  .querySelectorAll("div.target");
+}
